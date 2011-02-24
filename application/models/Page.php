c8ba963607239498cb37650d5940ede7e43d671f
Hector Virgen <hector.virgen@disney.com>
2011-02-24 12:21:02 -0800

    Updates controller to read page data from page object, which in turn pulls data form .json file.

--- application/models/Page.php
+++ application/models/Page.php
@@ -2,7 +2,7 @@
 
 class Default_Model_Page
 {
-    const PAGE = 'offers/page.xml';
+    protected $_uri;
     
     protected $_repo;
     
@@ -26,17 +26,94 @@ class Default_Model_Page
     
     public function save($data)
     {
-        if (!$this->_user) {
-            
-        }
-        $userRepo = $this->_getUserRepo();
+//        $branch = $this->_user ? $this->_user : 'master';
+//        $userRepo = $this->_getUserRepo();
+        $json = $ths->_toJson($data);
+    }
+    
+    public function publish()
+    {
         
     }
     
     public function getData()
     {
-        if (null === $this->_data) {
-            
-        }
+        $user = ($this->_user ? $this->_user : 'master');
+        $path = realpath(APPLICATION_PATH . "/../data/{$user}/{$this->_uri}/page.json");
+        $json = file_get_contents($path);
+        return json_decode($json, true);
     }
+    
+    public function _toJson($data)
+    { 
+        $tab = "  "; 
+        $new_json = ""; 
+        $indent_level = 0; 
+        $in_string = false; 
+        
+        $json = json_encode($data); 
+        $len = strlen($json); 
+    
+        for($c = 0; $c < $len; $c++) 
+        { 
+            $char = $json[$c]; 
+            switch($char) 
+            { 
+                case '{': 
+                case '[': 
+                    if(!$in_string) 
+                    { 
+                        $new_json .= $char . "\n" . str_repeat($tab, $indent_level+1); 
+                        $indent_level++; 
+                    } 
+                    else 
+                    { 
+                        $new_json .= $char; 
+                    } 
+                    break; 
+                case '}': 
+                case ']': 
+                    if(!$in_string) 
+                    { 
+                        $indent_level--; 
+                        $new_json .= "\n" . str_repeat($tab, $indent_level) . $char; 
+                    } 
+                    else 
+                    { 
+                        $new_json .= $char; 
+                    } 
+                    break; 
+                case ',': 
+                    if(!$in_string) 
+                    { 
+                        $new_json .= ",\n" . str_repeat($tab, $indent_level); 
+                    } 
+                    else 
+                    { 
+                        $new_json .= $char; 
+                    } 
+                    break; 
+                case ':': 
+                    if(!$in_string) 
+                    { 
+                        $new_json .= ": "; 
+                    } 
+                    else 
+                    { 
+                        $new_json .= $char; 
+                    } 
+                    break; 
+                case '"': 
+                    if($c > 0 && $json[$c-1] != '\\') 
+                    { 
+                        $in_string = !$in_string; 
+                    } 
+                default: 
+                    $new_json .= $char; 
+                    break;                    
+            } 
+        } 
+    
+        return $new_json; 
+    } 
 }
