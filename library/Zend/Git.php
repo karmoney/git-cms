c91166a0beb3566472540bf6a1fd9cef3706b1a3
karmoney <kartik.money@disney.com>
2011-02-24 09:01:28 -0800

    Added Git Library to interact with Git.

---
+++ library/Zend/Git.php
@@ -0,0 +1,31 @@
+<?php
+
+class Zend_Git {
+
+	/**
+	 * Create a new git repository
+	 *
+	 * Accepts a creation path, and, optionally, a source path
+	 *
+	 * @access  public
+	 * @param   string  repository path
+	 * @param   string  directory to source
+	 * @return  GitRepo
+	 */	
+	public static function &create($repo_path, $source = null) {
+		return Zend_Git_Repo::create_new($repo_path, $source);
+	}
+
+	/**
+	 * Open an existing git repository
+	 *
+	 * Accepts a repository path
+	 *
+	 * @access  public
+	 * @param   string  repository path
+	 * @return  GitRepo
+	 */	
+	public static function open($repo_path) {
+		return new Zend_Git_Repo($repo_path);
+	}
+}
