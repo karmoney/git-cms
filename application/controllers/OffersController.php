f9b381f6c9a6728e19c762af839d3d5c23965348
karmoney <kartik.money@disney.com>
2011-02-24 13:29:06 -0800

    added back change to offerscontroller

--- application/controllers/OffersController.php
+++ application/controllers/OffersController.php
@@ -18,7 +18,13 @@ class OffersController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        $page = new Default_Model_Page('offers');
+//        // Only set user if it is set
+//        $user = 'hector';
+//        $page->setUser($user);
         
+        $data = $page->getData();
+        $this->view->assign($data);
     }
 }
 
